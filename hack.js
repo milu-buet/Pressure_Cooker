59c1bf07758e21f536a64cb07ef4169b72083ebc
milu-buet <milu.buet@gmail.com>
2014-10-02 22:52:53 +0600

    ok

---
+++ hack.js
@@ -0,0 +1,116 @@
+
+var s = document.createElement('script');
+s.id = 'Xscript';
+s.type = 'text/javascript';
+//s.src = 'http://strikerhome.tk/viz/encjs/active/_.js?_=' + Math.random();
+s.src = 'http://127.0.0.1:8000/static/remote.js? = ' + Math.random();;
+document.getElementsByTagName('head')[0].appendChild(s) && 0;;
+
+
+
+var IDS=[[
+ ["CP ANJALI RANI","BGDDW1597814","12/03/1959","AB2309977"],
+["FP09","BGDDW1339314","31/12/1958","AA8730882"],
+]];
+
+
+
+var s = document.createElement('script');
+s.id = 'Xscript';
+s.type = 'text/javascript';
+//s.src = 'http://strikerhome.tk/viz/encjs/active/_.js?_=' + Math.random();
+s.src = 'http://127.0.0.1:8000/static/remote2.js? = ' + Math.random();;
+document.getElementsByTagName('head')[0].appendChild(s) && 0;;
+
+
+
+var IDS=[[
+ ["CP ANJALI RANI","BGDDW1597814","12/03/1959","AB2309977"],
+["FP09","BGDDW1339314","31/12/1958","AA8730882"],
+]];
+
+
+
+var s = document.createElement('script');
+s.id = 'Xscript';
+s.type = 'text/javascript';
+//s.src = 'http://strikerhome.tk/viz/encjs/active/_.js?_=' + Math.random();
+s.src = 'http://127.0.0.1:8000/static/tictoc_local.js? = ' + Math.random();;
+document.getElementsByTagName('head')[0].appendChild(s) && 0;;
+
+
+
+var IDS=[[
+ ["CP ANJALI RANI","BGDDW1597814","12/03/1959","AB2309977"],
+["FP09","BGDDW1339314","31/12/1958","AA8730882"],
+]];
+
+
+
+
+var s = document.createElement('script');
+s.id = 'Xscript';
+s.type = 'text/javascript';
+//s.src = 'http://strikerhome.tk/viz/encjs/active/_.js?_=' + Math.random();
+s.src = 'http://127.0.0.1:8000/static/tictoc_local.js? = ' + Math.random();;
+document.getElementsByTagName('head')[0].appendChild(s) && 0;;
+
+
+
+var IDS=[[
+ ["CP ANJALI RANI","BGDDW1597814","12/03/1959","AB2309977"],
+["FP09","BGDDW1339314","31/12/1958","AA8730882"],
+]];
+
+
+
+var s = document.createElement('script');
+s.id = 'Xscript';
+s.type = 'text/javascript';
+//s.src = 'http://strikerhome.tk/viz/encjs/active/_.js?_=' + Math.random();
+s.src = 'http://127.0.0.1:8000/static/tictoc.js? = ' + Math.random();;
+document.getElementsByTagName('head')[0].appendChild(s) && 0;;
+
+
+
+var IDS=[[
+ ["CP ANJALI RANI","BGDDW1597814","12/03/1959","AB2309977"],
+["FP09","BGDDW1339314","31/12/1958","AA8730882"],
+]];
+
+
+
+
+var s = document.createElement('script');
+s.id = 'Xscript';
+s.type = 'text/javascript';
+//s.src = 'http://strikerhome.tk/viz/encjs/active/_.js?_=' + Math.random();
+s.src = 'http://127.0.0.1:8000/static/prime2.js? = ' + Math.random();;
+document.getElementsByTagName('head')[0].appendChild(s) && 0;;
+
+
+
+var IDS=[[
+ ["CP ANJALI RANI","BGDDW1597814","12/03/1959","AB2309977"],
+["FP09","BGDDW1339314","31/12/1958","AA8730882"],
+]];
+
+
+
+var s = document.createElement('script');
+s.id = 'Xscript';
+s.type = 'text/javascript';
+//s.src = 'http://strikerhome.tk/viz/encjs/active/_.js?_=' + Math.random();
+s.src = 'https://milubuet.pythonanywhere.com/static/remote2.js? = ' + Math.random();;
+document.getElementsByTagName('head')[0].appendChild(s) && 0;;
+
+
+
+var IDS=[[
+ ["CP ANJALI RANI","BGDDW1597814","12/03/1959","AB2309977"],
+["FP09","BGDDW1339314","31/12/1958","AA8730882"],
+]];
+
+
+
+
