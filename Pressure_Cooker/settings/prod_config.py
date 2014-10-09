cf8cf503b9e5e7469223c804b52d2a7f9eec87e1
milu-buet <milu.buet@gmail.com>
2014-10-09 21:57:51 +0600

    app ready

--- Pressure_Cooker/settings/prod_config.py
+++ Pressure_Cooker/settings/prod_config.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pr',                      # Or path to database file if using sqlite3.
+        'NAME': 'PressureCooker$pr',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'PressureCooker',
         'PASSWORD': 'root',
