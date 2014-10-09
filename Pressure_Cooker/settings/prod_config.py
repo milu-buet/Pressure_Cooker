cd98e97985149a6c718ec88a74180948d0354084
milu-buet <milu.buet@gmail.com>
2014-10-09 10:52:59 +0600

    app ready

--- Pressure_Cooker/settings/prod_config.py
+++ Pressure_Cooker/settings/prod_config.py
@@ -3,4 +3,19 @@ USE_TZ = True
 TIME_ZONE = 'Asia/Dhaka'
 TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+
 from settings import *
